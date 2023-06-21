1efff066c4ae18ec2f1d92f346e4893de6e7624e
Brandon Wright <brandon.w.wright4@gmail.com>
2023-06-21 12:48:09 -0700

    add modulus conditional feature

--- palindrome-checker.js
+++ palindrome-checker.js
@@ -10,26 +10,29 @@
 
 function palindrome(str) {
   
-  let formattedString = str.split("\\").join("").match(/\w/g).join("").replace(/\_/g, '');
+  let formattedString = str.split("\\").join("").match(/\w/g).join("").replace(/\_/g, '').toLowerCase();
+  
   if (formattedString.length % 2 === 0) {
-    formattedString.split()
-    console.log(formattedString.split());
+    console.log("this is an even length string");
+    let newStr = formattedString.substring(formattedString.length / 2 - 1).split('').reverse().join('');
+    let regex = new RegExp(newStr);
+    return regex.test(formattedString);
   }
-  return true;
+  return false;
 
 }
 
-console.log(`${palindrome("eye")} should return a boolean.`);
-console.log(`${palindrome("eye")} should return true.`);
-console.log(`${palindrome("_eye")} should return true.`);
-console.log(`${palindrome("race car")} should return true.`);
-console.log(`${palindrome("not a palindrome")} should return false.`);
-console.log(`${palindrome("A man, a plan, a canal. Panama")} should return true.`);
-console.log(`${palindrome("never odd or even")} should return true.`);
-console.log(`${palindrome("nope")} should return false.`);
-console.log(`${palindrome("almostomla")} should return false.`);
-console.log(`${palindrome("My age is 0, 0 si ega ym.")} should return true.`);
-console.log(`${palindrome("1 eye for of 1 eye.")} should return false.`);
-console.log(`${palindrome("0_0 (: /-\ :) 0-0")} should return true.`);
-console.log(`${palindrome("five|\_/|four")} should return false.`);
+console.log(`${palindrome("eye")} should return a boolean. 1`);
+console.log(`${palindrome("eye")} should return true. 2`);
+console.log(`${palindrome("_eye")} should return true. 3`);
+console.log(`${palindrome("race car")} should return true. 4`);
+console.log(`${palindrome("not a palindrome")} should return false. 5`);
+console.log(`${palindrome("A man, a plan, a canal. Panama")} should return true. 6`);
+console.log(`${palindrome("never odd or even")} should return true. 7`);
+console.log(`${palindrome("nope")} should return false. 8`);
+console.log(`${palindrome("almostomla")} should return false. 9`);
+console.log(`${palindrome("My age is 0, 0 si ega ym.")} should return true. 10`);
+console.log(`${palindrome("1 eye for of 1 eye.")} should return false. 11`);
+console.log(`${palindrome("0_0 (: /-\ :) 0-0")} should return true. 12`);
+console.log(`${palindrome("five|\_/|four")} should return false. 13`);
 
